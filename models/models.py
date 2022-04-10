7e32b84304d6e631a2f9533c4c86e6a79cc2261a
Wei Zhang <zhangw@shinetechsoftware.com>
2022-04-10 15:54:07 +0800

    Added computed field

--- models/models.py
+++ models/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import timedelta
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
 
 class book_store(models.Model):
     _name = 'book_store.book'
@@ -44,6 +47,20 @@ class book_store(models.Model):
         domain=[],
     )
 
+    publisher_city = fields.Char(
+        'Publisher City',
+        related='publisher_id.city',
+        readonly=True)
+
+    age_days = fields.Float(
+        string='Days Since Release',
+        compute='_compute_age',
+        inverse='_inverse_age',
+        search='_search_age',
+        store=False,  # optional
+        compute_sudo=False  # optional
+    )
+
     @api.depends('value')
     def _value_pc(self):
         for record in self:
@@ -64,7 +81,8 @@ class book_store(models.Model):
             if book.is_allowed_transition(book.state, new_state):
                 book.state = new_state
             else:
-                continue
+                msg = _('Moving from %s to %s is not allowed') % (book.state, new_state)
+                raise UserError(msg)
 
     def make_available(self):
         self.change_state('available')
@@ -74,6 +92,35 @@ class book_store(models.Model):
 
     def make_lost(self):
         self.change_state('lost')
+
+    @api.depends('date_release')
+    def _compute_age(self):
+        today = fields.Date.today()
+        for book in self:
+            if book.date_release:
+                delta = today - book.date_release
+                book.age_days = delta.days
+            else:
+                book.age_days = 0
+
+    def _inverse_age(self):
+        today = fields.Date.today()
+        for book in self.filtered('date_release'):
+            d = today - timedelta(days=book.age_days)
+            book.date_release = d
+
+    def _search_age(self, operator, value):
+        today = fields.Date.today()
+        value_days = timedelta(days=value)
+        value_date = today - value_days
+        # 运算符转换：
+        # age_days > value -> date < value_date
+        operator_map = {
+            '>': '<', '>=': '<=',
+            '<': '>', '<=': '>=',
+        }
+        new_op = operator_map.get(operator, operator)
+        return [('date_release', new_op, value_date)]
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -87,3 +134,8 @@ class ResPartner(models.Model):
         string='Authored Books',
         # relation='library_book_res_partner_rel' # optional
     )
+
+    @api.depends('authored_book_ids')
+    def _compute_count_books(self):
+        for r in self:
+            r.count_books = len(r.authored_book_ids)
